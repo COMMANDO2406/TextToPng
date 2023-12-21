8284295c63d2fd51017bd8ebe1ee2dd871e9f146
COMMANDO2406 <raghavsethi.in@gmail.com>
2023-12-21 20:16:16 +0530

    Added textual

--- src/main.py
+++ src/main.py
@@ -1,20 +1,33 @@
-from PIL import Image
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, Static
 
-test_str = input("Enter the text to be converted: \n")
+class BinaryConverter(App):
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode"), ("ctrl-c", "quit", "Quit the application")]
 
-res = ''.join(format(ord(i), '08b') for i in test_str)
-print("The string after binary conversion : " + str(res))
+    def action_toggle_dark(self) -> None:
+        self.dark = not self.dark
+    
+    def on_key(self, event) -> None:
+        if event.key == "enter":
+            # Process the user input when Enter key is pressed
+            self.process_input()
+        elif event.key == "backspace":
+            # Handle backspace to delete characters
+            self.input_text = self.input_text[:-1]
+        else:
+            # Append other keys to the input text
+            self.input_text += event.text
 
-pixels = []
-
-for i in range(0, len(res), 8):
-    pixels.append(list(map(int, res[i:i+8])))
-
-print(pixels)
-print(len(res))
-
-img = Image.new('1', (8, len(pixels)))
-img.putdata([pixel for row in pixels for pixel in row])
-img.save('output_image.png')
-
-input("Press Enter to exit...")
+    def process_input(self) -> None:
+        # Process the user input (you can replace this with your logic)
+        print("User input:", self.input_text)
+        # Clear the input for the next user input
+        self.input_text = ""
+        
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+    
+if __name__ == "__main__":
+    app = BinaryConverter()
+    app.run()
