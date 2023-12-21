e1b2f133b068f16815d850f59496ba01e97a361b
COMMANDO2406 <raghavsethi.in@gmail.com>
2023-12-21 20:35:44 +0530

    Added textual basics

--- src/main.py
+++ src/main.py
@@ -6,24 +6,6 @@ class BinaryConverter(App):
 
     def action_toggle_dark(self) -> None:
         self.dark = not self.dark
-    
-    def on_key(self, event) -> None:
-        if event.key == "enter":
-            # Process the user input when Enter key is pressed
-            self.process_input()
-        elif event.key == "backspace":
-            # Handle backspace to delete characters
-            self.input_text = self.input_text[:-1]
-        else:
-            # Append other keys to the input text
-            self.input_text += event.text
-
-    def process_input(self) -> None:
-        # Process the user input (you can replace this with your logic)
-        print("User input:", self.input_text)
-        # Clear the input for the next user input
-        self.input_text = ""
-        
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
